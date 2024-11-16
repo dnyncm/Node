d88e1aa6a6ab8cf143bcdf92ccc6653fbf590ae0
dnyncm <dny.ncm@gmail.com>
2024-11-16 18:10:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
+//create new file
 /**fs.writeFile('message.txt', 'Hello from NODE JS', (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
   });**/
 
+//read/cosole log file. Use 'utf8' to decode. 
 fs.readFile("./message.txt", 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
